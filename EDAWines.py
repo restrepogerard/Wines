d9a7b32bcea72a4e5125868889962d508c3f7cb1
luis restrepo <restrepo.gerard@gmail.com>
2025-10-25 16:30:41 -0500

    segmentar el EDA

--- EDAWines.py
+++ EDAWines.py
@@ -1,4 +1,13 @@
-# carga incial
-#cambio de luis
-# otra linea de cambio
-# pip install numpy
+# Librerias y carga de datos
+
+# ETL Exploratorio de datos de vinos
+
+#Identificación de datos faltantes, duplicados y tipos de datos incorrectos
+
+# Análisis estadístico descriptivo
+# Visualización de datos
+# Identificación de outliers
+# Correlación entre variables
+# Preparación de datos para modelado
+# Analisis multivariado
+# limpieza y transformación de datos
